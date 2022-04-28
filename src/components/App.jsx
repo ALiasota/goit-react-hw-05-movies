1d476def977b6af3aed61db9f3200ecc189beed8
Andrii <Andrii@iMac.local>
2022-04-28 19:52:14 +0300

    git

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { lazy, Suspense } from 'react';
+
 export const App = () => {
   return (
     <div
@@ -11,7 +13,7 @@ export const App = () => {
         color: '#010101',
       }}
     >
-      React homework template
+      React homework
     </div>
   );
 };
